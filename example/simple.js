5f631152bb659f61ed814f48394bd5f1ddc41c82
Lukas Zech <lukas-zech-software@users.noreply.github.com>
2015-01-27 18:28:03 +0100

    Initial commit

---
+++ example/simple.js
@@ -0,0 +1,13 @@
+/*
+ * period-format
+ * user/repo
+ *
+ * Copyright (c) 2015
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var periodFormat = require('../');
+
+console.log(periodFormat()); // "awesome"
---
+++ test/period-format_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var periodFormat = require('../');
+var assert = require('should');
+
+describe('periodFormat', function () {
+
+  it('should be awesome', function () {
+    periodFormat().should.equal('awesome');
+  });
+
+});
